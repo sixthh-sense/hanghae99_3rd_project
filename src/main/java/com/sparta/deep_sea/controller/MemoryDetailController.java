684b50649fd2a3c0ce1b8621ad7bf26d41344308
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2021-11-22 05:22:36 +0900

    수정_thymeleaf_onlycontroller

--- src/main/java/com/sparta/deep_sea/controller/MemoryDetailController.java
+++ src/main/java/com/sparta/deep_sea/controller/MemoryDetailController.java
@@ -5,28 +5,23 @@ import com.sparta.deep_sea.domain.MemoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class MemoryDetailController {
     private final MemoryRepository memoryRepository;
 
-    @RequestMapping(value="/detail/{id}", method= RequestMethod.GET)
+    @GetMapping("/detail/{id}")
     public String moreMemory(@PathVariable Long id, Model model) {
-        Optional<Memory> memory = memoryRepository.findById(id);
-        if (!memory.isPresent()) {
-            return "redirect:/";
-        }
-        model.addAttribute("title", memory.get().getTitle());
-        model.addAttribute("name", memory.get().getName());
-        model.addAttribute("comment", memory.get().getComment());
+        Memory memory = memoryRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("ID가 존재하지 않습니다."));
+        System.out.println(model);
+        model.addAttribute("title", memory.getTitle());
+        model.addAttribute("name", memory.getName());
+        model.addAttribute("comment", memory.getComment());
+        System.out.println(model);
         return "detail";
     }
 }
-
-//
